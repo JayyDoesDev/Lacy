0067c2186590c5f715ad2e0d968566c585b7f66d
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2024-02-14 23:42:35 -0500

    chore: Libraries added to exclude from compiler

--- Common/UserExists.ts
+++ Common/UserExists.ts
@@ -1,8 +1,7 @@
 import UserSchema from "../Models/UserSchema";
 import type { Snowflake } from "@antibot/interactions";
 export async function UserExists(userId: Snowflake): Promise<boolean> {
-  const findUser = await UserSchema.findOne({ User: userId });
   let bool: boolean;
-  findUser ? bool = true : bool = false;
+  await UserSchema.findOne({ User: userId }) ? bool = true : bool = false;
   return bool;
 }
