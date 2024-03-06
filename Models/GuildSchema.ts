daebf9c262392c1b00d6b537eaf003b7734a2a5a
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2024-03-05 22:56:13 -0500

    chore: Readying for the tags feature.

--- Models/GuildSchema.ts
+++ Models/GuildSchema.ts
@@ -5,17 +5,40 @@ interface AutoResponders {
   AutoResponderResponse: String;
   Date: Date;
 }
+
+interface Tags {
+  TagTitle: String;
+  TagName: String;
+  TagResponse: String;
+  TagFooter: String;
+  Date: Date;
+}
+
 interface GuildDocument extends Document {
   Guild: String;
   AutoResponders: AutoResponders[];
+  Tags: Tags[];
 }
+
+
 
 const GuildSchema = new Schema({
   Guild: {
     type: String
   },
   AutoResponders: {
-    type: [ { AutoResponderName: String, AutoResponderResponse: String, Date: Date } ],
+    type: [{ AutoResponderName: String, AutoResponderResponse: String, Date: Date }],
+    default: []
+  },
+  Tags: {
+    type: [
+      {
+        TagTitle: { type: String, default: null },
+        TagName: { type: String },
+        TagResponse: { type: String, default: null },
+        TagFooter: { type: String, default: null }
+      }
+    ],
     default: []
   }
 }, { versionKey: false });
---
+++ Plugins/Tags/TagsPlugin.ts
@@ -0,0 +1,8 @@
+import { DefinePlugin, Plugin } from "../../Common/DefinePlugin";
+
+export = DefinePlugin({
+  name: "Tags",
+  description: "Create tags to use as mini commands in your server!",
+  commands: [],
+  public_plugin: true
+}) as Plugin;
