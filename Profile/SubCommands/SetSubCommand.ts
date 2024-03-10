55ca50dc26130b2b485eeeec2471cda46bc06429
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2024-03-09 21:07:02 -0500

    Update SetSubCommand.ts

--- Profile/SubCommands/SetSubCommand.ts
+++ Profile/SubCommands/SetSubCommand.ts
@@ -3,6 +3,7 @@ import { Context } from "../../Context";
 import { ChatInputCommandInteraction, ModalBuilder, ModalSubmitInteraction, TextInputStyle } from "discord.js";
 import { Colors } from "../../Common/Colors";
 import { ActionRowBuilder, TextInputBuilder } from "@discordjs/builders";
+import { RegisterSubCommand } from "../../Common/RegisterSubCommand";
 export const ProfileSetSubCommand: ApplicationCommandOptions = {
   name: "set",
   description: "Set your pronouns and description!",
@@ -10,36 +11,41 @@ export const ProfileSetSubCommand: ApplicationCommandOptions = {
   options: []
 } as ApplicationCommandOptions;
 
-export async function RunProfileSetSubCommand(ctx: Context, interaction: ChatInputCommandInteraction): Promise<void> {
-  const modal: ModalBuilder = new ModalBuilder()
-    .setCustomId(`profile_${interaction.user.id}`)
-    .setTitle("Setup Your Global Profile")
-  const preferredName: TextInputBuilder = new TextInputBuilder()
-    .setCustomId('preferred_name')
-    .setLabel("What is your preferred name?")
-    .setPlaceholder("Lacy")
-    .setStyle(TextInputStyle.Short)
-    .setMaxLength(60)
-    .setRequired(false);
-  const pronouns: TextInputBuilder = new TextInputBuilder()
-    .setCustomId("profile_pronouns")
-    .setLabel("What is your pronouns?")
-    .setPlaceholder("she/her")
-    .setStyle(TextInputStyle.Short)
-    .setMaxLength(15)
-    .setRequired(false);
-  const description: TextInputBuilder = new TextInputBuilder()
-    .setCustomId('profile_description')
-    .setLabel("What do you want people to know about you?")
-    .setPlaceholder("I like to play games!")
-    .setStyle(TextInputStyle.Paragraph)
-    .setMaxLength(300)
-    .setRequired(false);
-  const preferredNameRow: ActionRowBuilder<TextInputBuilder> = new ActionRowBuilder<TextInputBuilder>().addComponents(preferredName);
-  const pronounsRow: ActionRowBuilder<TextInputBuilder> = new ActionRowBuilder<TextInputBuilder>().addComponents(pronouns);
-  const descriptionRow: ActionRowBuilder<TextInputBuilder> = new ActionRowBuilder<TextInputBuilder>().addComponents(description);
-  modal.addComponents(preferredNameRow, pronounsRow, descriptionRow as any)
-  if (interaction.options.getSubcommand() === "set") {
-    await interaction.showModal(modal);
-  }
+export function RunProfileSetSubCommand(ctx: Context, interaction: ChatInputCommandInteraction): void {
+  RegisterSubCommand({
+    subCommand: "set",
+    ctx: ctx,
+    interaction: interaction,
+    callback: async (ctx: Context, interaction: ChatInputCommandInteraction) => {
+      const modal: ModalBuilder = new ModalBuilder()
+        .setCustomId(`profile_${interaction.user.id}`)
+        .setTitle("Setup Your Global Profile")
+      const preferredName: TextInputBuilder = new TextInputBuilder()
+        .setCustomId('preferred_name')
+        .setLabel("What is your preferred name?")
+        .setPlaceholder("Lacy")
+        .setStyle(TextInputStyle.Short)
+        .setMaxLength(60)
+        .setRequired(false);
+      const pronouns: TextInputBuilder = new TextInputBuilder()
+        .setCustomId("profile_pronouns")
+        .setLabel("What is your pronouns?")
+        .setPlaceholder("she/her")
+        .setStyle(TextInputStyle.Short)
+        .setMaxLength(15)
+        .setRequired(false);
+      const description: TextInputBuilder = new TextInputBuilder()
+        .setCustomId('profile_description')
+        .setLabel("What do you want people to know about you?")
+        .setPlaceholder("I like to play games!")
+        .setStyle(TextInputStyle.Paragraph)
+        .setMaxLength(300)
+        .setRequired(false);
+      const preferredNameRow: ActionRowBuilder<TextInputBuilder> = new ActionRowBuilder<TextInputBuilder>().addComponents(preferredName);
+      const pronounsRow: ActionRowBuilder<TextInputBuilder> = new ActionRowBuilder<TextInputBuilder>().addComponents(pronouns);
+      const descriptionRow: ActionRowBuilder<TextInputBuilder> = new ActionRowBuilder<TextInputBuilder>().addComponents(description);
+      modal.addComponents(preferredNameRow, pronounsRow, descriptionRow as any)
+      await interaction.showModal(modal);
+    }
+  })
 }
